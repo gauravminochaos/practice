7489b35d2bfbe70acbcc3d5214abc144ea58a82f
Gaurav <gaurav.minocha.os@gmail.com>
2018-04-01 22:51:42 -0700

    Adding functionality to minute

--- timer.js
+++ timer.js
@@ -1,81 +1,94 @@
-  var count, counting = 0;
-  var interval, timeout;
-  var s_up, s_down, s_display;
-  var audio_file = 'beep.mp3';
-  var max = 300;
+var s_up, s_down, s_display;
+var max = 300;
+var second_counter, minute_counter;
 
-  window.onload = function() {
-    s_up = document.getElementById('s_up');
-    s_down = document.getElementById('s_down');
-    s_display = document.getElementById('s_display');
+window.onload = function() {
+  s_up = document.getElementById('s_up');
+  s_down = document.getElementById('s_down');
+  s_display = document.getElementById('s_display');
+  second_counter = new counter();
+  second_counter.hookup(s_display, s_up, s_down, 60);
 
-    s_down.onmousedown = function() {
-      timeout = setInterval(function() {
-        handle_down(s_display)
-      }, 100)
-    };
-    s_down.onmouseup = function() {
-      clearInterval(timeout);
-    };
-    s_up.onmousedown = function() {
-      timeout = setInterval(function() {
-        handle_up(s_display, 300)
-      }, 100)
-    };
-    s_up.onmouseup = function() {
-      clearInterval(timeout);
-    };
+  m_up = document.getElementById('m_up');
+  m_down = document.getElementById('m_down');
+  m_display = document.getElementById('m_display');
+  minute_counter = new counter();
+  minute_counter.hookup(m_display, m_up, m_down, 60);
+}
 
-    s_display.onclick = function() {
-      handle_display_press(s_display)
-    };
-    s_up.onclick = function() {
-      handle_up(s_display, 300)
-    };
-    s_down.onclick = function() {
-      handle_down(s_display)
-    };
-  }
+function counter() {
+  var self = this;
 
-  function handle_display_press(display) {
-    count = display.innerText;
-    if (count == 0)
-      return;
+  this.hookup = function(display, up, down, max) {
+    self.display = display;
+    self.up = up;
+    self.down = down;
+    self.audio_file = 'beep.mp3';
+    self.max = max;
+    self.counting = 0;
+    self.interval;
+    self.timeout;
 
-    if (counting) {
-      counting = 0;
-      clearInterval(interval);
+    display.onclick = self.handle_display_press;
+    up.onclick = self.handle_up;
+    down.onclick = self.handle_down;
+    up.onmouseup = self.handle_mouseup;
+    up.onmousedown = self.handle_up_mousedown;
+    down.onmouseup = self.handle_mouseup;
+    down.onmousedown = self.handle_down_mousedown;
+  };
+
+  this.handle_display_press = function() {
+    if (self.counting) {
+      self.counting = 0;
+      clearInterval(self.interval);
     } else {
-      counting = 1;
-      interval = setInterval(function() {
-        decrement(display)
+      self.counting = 1;
+      self.interval = setInterval(function() {
+        self.decrement();
       }, 1000);
     }
-  }
+  };
 
-  function decrement(display) {
+  this.decrement = function() {
+    count = Number(self.display.innerText);
     count = count - 1 > 0 ? count - 1 : 0;
     if (count == 0) {
-      counting = 0;
-      clearInterval(interval);
-      play_sound();
+      self.counting = 0;
+      clearInterval(self.interval);
+      self.play_sound();
     }
-    display.innerText = count < 10 ? '0' + count : count;;
-  }
+    self.display.innerText = count < 10 ? '0' + count : count;;
+  };
 
-  function play_sound() {
-    var audio = new Audio(audio_file);
+  this.play_sound = function() {
+    var audio = new Audio(self.audio_file);
     audio.play();
-  }
+  };
 
-  function handle_up(display, max) {
-    var new_count = Number(display.innerText) + 1;
-    new_count = new_count <= max ? new_count : max;
-    display.innerText = new_count < 10 ? '0' + new_count : new_count;
-  }
+  this.handle_up = function() {
+    var new_count = Number(self.display.innerText) + 1;
+    new_count = new_count <= self.max ? new_count : self.max;
+    self.display.innerText = new_count < 10 ? '0' + new_count : new_count;
+  };
 
-  function handle_down(display) {
-    var new_count = Number(display.innerText) - 1;
+  this.handle_down = function() {
+    var new_count = Number(self.display.innerText) - 1;
     new_count = new_count > 0 ? new_count : 0;
-    display.innerText = new_count < 10 ? '0' + new_count : new_count;
-  }
+    self.display.innerText = new_count < 10 ? '0' + new_count : new_count;
+  };
+
+  this.handle_up_mousedown = function() {
+    self.timeout = setInterval(function() {
+      self.handle_up();
+    }, 100)
+  };
+
+  this.handle_down_mousedown = function() {
+    self.timeout = setInterval(self.handle_down(), 100)
+  };
+
+  this.handle_mouseup = function() {
+    clearInterval(self.timeout);
+  };
+}
