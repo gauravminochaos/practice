4dd8a398979ce6aa9e95f77dea689dcb707c9ac7
Gaurav <gaurav.minocha.os@gmail.com>
2018-04-03 23:01:25 -0700

    cleaned

--- timer/timer.js
+++ timer/timer.js
@@ -1,28 +1,28 @@
 var s_up, s_down, s_display;
-var max = 300;
 var second_counter, minute_counter;
 var counting;
-var minute_val;
-var second_val;
 var s_count, m_count;
-var audio_file = 'beep.mp3';
+var audio_file;
+
 window.onload = function() {
   counting = 0;
-  minute_val = 1;
-  second_val = 30;
+  audio_file = "beep.mp3"
+
   s_up = document.getElementById('s_up');
   s_down = document.getElementById('s_down');
   s_display = document.getElementById('s_display');
   second_counter = new counter();
   second_counter.hookup(s_display, s_up, s_down, 60);
-  s_count = Number(s_display.innerText);
-  m_count = Number(m_display.innerText);
 
   m_up = document.getElementById('m_up');
   m_down = document.getElementById('m_down');
   m_display = document.getElementById('m_display');
   minute_counter = new counter();
   minute_counter.hookup(m_display, m_up, m_down, 60);
+
+  s_count = Number(s_display.innerText);
+  m_count = Number(m_display.innerText);
+
   m_display.onclick = handle_display_press;
   s_display.onclick = handle_display_press;
 }
@@ -42,29 +42,26 @@ handle_display_press = function() {
 decrement = function() {
   s_count = Number(s_display.innerText);
   s_count = s_count - 1 > 0 ? s_count - 1 : 0;
-  s_display.innerText = s_count < 10 ? '0' + s_count : s_count;;
+  s_display.innerText = s_count < 10 ? '0' + s_count : s_count;
+
   if (s_count == 0) {
     m_count = Number(m_display.innerText);
     if (m_count != 0) {
       m_count = m_count - 1;
       m_display.innerText = m_count < 10 ? '0' + m_count : m_count;
       s_display.innerText = 60;
-    }
-    else if (m_count == 0) {
+    } else if (m_count == 0) {
       counting = 0;
       clearInterval(self.interval);
       play_sound();
     }
   }
-  // format it
-
 };
 
 play_sound = function() {
   var audio = new Audio(audio_file);
   audio.play();
 };
-
 
 function counter() {
   var self = this;
@@ -74,19 +71,17 @@ function counter() {
     self.up = up;
     self.down = down;
 
-    self.interval;
     self.max = max;
+    self.interval;
     self.timeout;
-
 
     up.onclick = self.handle_up;
     down.onclick = self.handle_down;
     up.onmouseup = self.handle_mouseup;
-    up.onmousedown = self.handle_up_mousedown;
     down.onmouseup = self.handle_mouseup;
+    up.onmousedown = self.handle_up_mousedown;
     down.onmousedown = self.handle_down_mousedown;
   };
-
 
   this.handle_up = function() {
     var new_count = Number(self.display.innerText) + 1;
